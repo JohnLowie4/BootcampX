64159ce6ab639a0eb534e3e3baa80b8efbf22d3d
John <jzlowie@gmail.com>
2021-06-23 23:56:55 -0600

    SQL injection assignment commit

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -20,15 +20,17 @@ const pool = new Pool({
 //   .catch(err => console.error('query error', err.stack));
 
 const cohort_name = process.argv[2];
-const max_result = process.argv[3];
+const max_result = process.argv[3] || 5;
+const values = [`%${cohort_name}%`, max_result];
 
-pool.query(`
-  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-  FROM students
-  JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name LIKE '%${cohort_name}%'
-  LIMIT ${max_result || 5};
-`)
+const queryString = `
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,19 +9,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const cohort_name = process.argv[2];
+const cohort_name = process.argv[2] || 'JUL02';
+const values = [`${cohort_name}`];
+const queryString = `
+SELECT DISTINCT
+teachers.name as teacher,
+cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY teacher;
+`;
 
-pool.query(`
-  SELECT DISTINCT
-  teachers.name as teacher,
-  cohorts.name as cohort
-  FROM teachers
-  JOIN assistance_requests ON teacher_id = teachers.id
-  JOIN students ON student_id = students.id
-  JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name = '${cohort_name}'
-  ORDER BY teacher;
-`)
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
